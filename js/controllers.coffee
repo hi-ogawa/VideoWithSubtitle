3644364b323413715641b0e91072e8a995c82705
hiogawa <hi.ogawa.zz@gmail.com>
2015-07-29 02:58:48 +0900

    put loading icon

--- js/controllers.coffee
+++ js/controllers.coffee
@@ -9,6 +9,7 @@ videosubApp.controller 'videosubCtrl', ['$scope', '$sce', 'getHTMLwithYQL', 'par
   $scope.titleQuery = ''
   $scope.x = 1
   $scope.y = 0
+  $scope.subtitle = ''
 
   # template urls
   url0 = (query) ->
@@ -25,12 +26,16 @@ videosubApp.controller 'videosubCtrl', ['$scope', '$sce', 'getHTMLwithYQL', 'par
   # show searched tvshow titles
   $scope.searchTitles = () ->
 
+    $scope.loading0 = true
     getHTMLwithYQL(url0($scope.titleQuery)).then (html) ->
+      $scope.loading0 = false
       items = parsers.getTitlesFromSpringfield html
       $scope.springfieldTitleSuggestions = items
       $scope.springfieldTitle = items[0].val
 
+    $scope.loading1 = true
     getHTMLwithYQL(url1($scope.titleQuery)).then (html) ->
+      $scope.loading1 = false
       items = parsers.getTitlesFromTVOnline html
       $scope.tvonlineTitleSuggestions = items
       $scope.tvonlineTitle = items[0].val
@@ -39,43 +44,57 @@ videosubApp.controller 'videosubCtrl', ['$scope', '$sce', 'getHTMLwithYQL', 'par
   # show available episodes on Springfield
   $scope.$watch 'springfieldTitle', (newValue, oldValue) ->
 
-    getHTMLwithYQL(url2(newValue)).then (html) ->
-      items = parsers.getEpisodesFromSpringfield html
-      $scope.springfieldEpisodes = items
-      $scope.springfieldEpisode  = items[0].val
+    if newValue
+      $scope.loading2 = true
+      getHTMLwithYQL(url2(newValue)).then (html) ->
+        $scope.loading2 = false
+        items = parsers.getEpisodesFromSpringfield html
+        $scope.springfieldEpisodes = items
+        $scope.springfieldEpisode  = items[0].val
 
   # show available episodes on TVOnline
   $scope.$watch 'tvonlineTitle', (newValue, oldValue) ->
 
-    getHTMLwithYQL(url3(newValue)).then (html) ->
-      items = parsers.getEpisodesFromTVOnline html
-      $scope.tvonlineEpisodes = items
-      $scope.tvonlineEpisode  = items[0].val
+    if newValue
+      $scope.loading3 = true
+      getHTMLwithYQL(url3(newValue)).then (html) ->
+        $scope.loading3 = false
+        items = parsers.getEpisodesFromTVOnline html
+        $scope.tvonlineEpisodes = items
+        $scope.tvonlineEpisode  = items[0].val
 
   # show available videos of certain episode on TVOnline
   $scope.$watch 'tvonlineEpisode', (newValue, oldValue) ->
 
-    getHTMLwithYQL(url3(newValue)).then (html) ->
-      comp = (i, j) ->
-        if i.name is 'nowvideo' or i.name is 'movshare' then -1 else 1
-      items = parsers.getEmbedVideos(html).sort comp
-      $scope.embedVideoUrls = items
-      $scope.embedVideoUrl  = items[0].val
+    if newValue
+      $scope.loading4 = true
+      getHTMLwithYQL(url3(newValue)).then (html) ->
+        $scope.loading4 = false
+        comp = (i, j) ->
+          if i.name is 'nowvideo' or i.name is 'movshare' then -1 else 1
+        items = parsers.getEmbedVideos(html).sort comp
+        $scope.embedVideoUrls = items
+        $scope.embedVideoUrl  = items[0].val
 
   # show subtitle of certain episode on Springfield
   $scope.$watch 'springfieldEpisode', (newValue, oldValue) ->
 
-    getHTMLwithYQL(url4(newValue)).then (html) ->
-      $scope.subtitle = parsers.getSubtitle html
-
+    if newValue
+      $scope.loading5 = true
+      getHTMLwithYQL(url4(newValue)).then (html) ->
+        $scope.loading5 = false
+        $scope.subtitle = parsers.getSubtitle html
 
   # to put untrasted resource into iframe
   $scope.trustSrc = (src) ->
     $sce.trustAsResourceUrl src
 
-  $scope.buttonColor = (x, y, x_, y_) ->
-    if x is x_ and y is y_ then 'btn-primary' else 'btn-default'
 
+  # change buttons color
+  $scope.buttonColor = (x, y, x_, y_) ->
+    if x is x_ and y is y_ then 'btn-warning' else 'btn-default'
+
+  # change the place for subtitle based on button press
   $scope.position = (x, y) ->
     "subtitle-position-#{x.toString()}#{y.toString()}"
 ]
--- js/controllers.js
+++ js/controllers.js
@@ -11,6 +11,7 @@
       $scope.titleQuery = '';
       $scope.x = 1;
       $scope.y = 0;
+      $scope.subtitle = '';
       url0 = function(query) {
         return "http://www.springfieldspringfield.co.uk/tv_show_episode_scripts.php?search=" + query;
       };
@@ -27,61 +28,81 @@
         return "http://www.springfieldspringfield.co.uk/" + path;
       };
       $scope.searchTitles = function() {
+        $scope.loading0 = true;
         getHTMLwithYQL(url0($scope.titleQuery)).then(function(html) {
           var items;
+          $scope.loading0 = false;
           items = parsers.getTitlesFromSpringfield(html);
           $scope.springfieldTitleSuggestions = items;
           return $scope.springfieldTitle = items[0].val;
         });
+        $scope.loading1 = true;
         return getHTMLwithYQL(url1($scope.titleQuery)).then(function(html) {
           var items;
+          $scope.loading1 = false;
           items = parsers.getTitlesFromTVOnline(html);
           $scope.tvonlineTitleSuggestions = items;
           return $scope.tvonlineTitle = items[0].val;
         });
       };
       $scope.$watch('springfieldTitle', function(newValue, oldValue) {
-        return getHTMLwithYQL(url2(newValue)).then(function(html) {
-          var items;
-          items = parsers.getEpisodesFromSpringfield(html);
-          $scope.springfieldEpisodes = items;
-          return $scope.springfieldEpisode = items[0].val;
-        });
+        if (newValue) {
+          $scope.subtitle = $scope.loading2 = true;
+          return getHTMLwithYQL(url2(newValue)).then(function(html) {
+            var items;
+            $scope.loading2 = false;
+            items = parsers.getEpisodesFromSpringfield(html);
+            $scope.springfieldEpisodes = items;
+            return $scope.springfieldEpisode = items[0].val;
+          });
+        }
       });
       $scope.$watch('tvonlineTitle', function(newValue, oldValue) {
-        return getHTMLwithYQL(url3(newValue)).then(function(html) {
-          var items;
-          items = parsers.getEpisodesFromTVOnline(html);
-          $scope.tvonlineEpisodes = items;
-          return $scope.tvonlineEpisode = items[0].val;
-        });
+        if (newValue) {
+          $scope.loading3 = true;
+          return getHTMLwithYQL(url3(newValue)).then(function(html) {
+            var items;
+            $scope.loading3 = false;
+            items = parsers.getEpisodesFromTVOnline(html);
+            $scope.tvonlineEpisodes = items;
+            return $scope.tvonlineEpisode = items[0].val;
+          });
+        }
       });
       $scope.$watch('tvonlineEpisode', function(newValue, oldValue) {
-        return getHTMLwithYQL(url3(newValue)).then(function(html) {
-          var comp, items;
-          comp = function(i, j) {
-            if (i.name === 'nowvideo' || i.name === 'movshare') {
-              return -1;
-            } else {
-              return 1;
-            }
-          };
-          items = parsers.getEmbedVideos(html).sort(comp);
-          $scope.embedVideoUrls = items;
-          return $scope.embedVideoUrl = items[0].val;
-        });
+        if (newValue) {
+          $scope.loading4 = true;
+          return getHTMLwithYQL(url3(newValue)).then(function(html) {
+            var comp, items;
+            $scope.loading4 = false;
+            comp = function(i, j) {
+              if (i.name === 'nowvideo' || i.name === 'movshare') {
+                return -1;
+              } else {
+                return 1;
+              }
+            };
+            items = parsers.getEmbedVideos(html).sort(comp);
+            $scope.embedVideoUrls = items;
+            return $scope.embedVideoUrl = items[0].val;
+          });
+        }
       });
       $scope.$watch('springfieldEpisode', function(newValue, oldValue) {
-        return getHTMLwithYQL(url4(newValue)).then(function(html) {
-          return $scope.subtitle = parsers.getSubtitle(html);
-        });
+        if (newValue) {
+          $scope.loading5 = true;
+          return getHTMLwithYQL(url4(newValue)).then(function(html) {
+            $scope.loading5 = false;
+            return $scope.subtitle = parsers.getSubtitle(html);
+          });
+        }
       });
       $scope.trustSrc = function(src) {
         return $sce.trustAsResourceUrl(src);
       };
       $scope.buttonColor = function(x, y, x_, y_) {
         if (x === x_ && y === y_) {
-          return 'btn-primary';
+          return 'btn-warning';
         } else {
           return 'btn-default';
         }
